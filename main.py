d258405d8589eab2e0b578655de4cc08b007aed2
Youness <std.youness@gmail.com>
2021-05-20 00:36:27 +0200

    First commit.

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5000"
+]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/{nom}')
+async def index(nom):
+    return {"msg": nom}
+
