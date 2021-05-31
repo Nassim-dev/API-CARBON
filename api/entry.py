d450846ff2c5587187c9c70aca545cff97a9bfb4
Youness <std.youness@gmail.com>
2021-05-31 20:32:56 +0200

    Set up twitter api completed.

--- api/entry.py
+++ api/entry.py
@@ -1,20 +1,26 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-origins = [
-    "http://localhost:5000"
-]
+import tweepy
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-    return {"msg": "J'adore le chocolat noir."}
+
+auth = tweepy.AppAuthHandler("ynLU7uJVRqWAmiqPdw8KSqxPu", "IByfALAjERVHR3Cr9pXp2eUkihyFimgOiyynR7DL0XDrvVjfHx")
+api = tweepy.API(auth)
+
+
+@app.get("/{user}")
+def read_root(user):
+    public_tweets = api.user_timeline(user, count=10)
+    tweets = []
+    for tweet in public_tweets:
+        tweets.append(tweet.text)
+    return {"msg": tweets}
